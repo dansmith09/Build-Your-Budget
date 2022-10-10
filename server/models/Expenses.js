001ba0a211678019f3a3d33bac707203d2081feb
dansmith09 <dansmith_09@hotmail.com>
2022-10-10 23:29:25 +1100

    Folder outline and models and config complete

---
+++ server/models/Expenses.js
@@ -0,0 +1,14 @@
+const { Schema } = require('mongoose');
+
+const expensesSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  expense: {
+    type: Int,
+    required: true,
+  },
+});
+
+module.exports = expensesSchema;
