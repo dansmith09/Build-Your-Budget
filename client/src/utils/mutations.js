70defdcfd35e65b49602111b835e7913eaae36ea
brake <riaconoo@icloud.com>
2022-10-11 21:28:01 +1100

    updated REMOVE_EXPENSE mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -37,28 +37,28 @@ mutation addExpense($expenseId: ID!, $expense: String!) {
 }
 
 `;
-    // TODO: Check line464 - expenses or expense?
-export const REMOVE_EXPENSE = gql `
-    mutation removeExpense($expense = String!) {
-        removeExpense(expense: $expense) {
-            _id
-            name
-            expense
+//     // TODO: Check line464 - expenses or expense?
+// export const REMOVE_EXPENSE = gql `
+//     mutation removeExpense($expense = String!) {
+//         removeExpense(expense: $expense) {
+//             _id
+//             name
+//             expense
+//         }
+//     }
+// `;
+// Mutation to use if we decide to remove an expense via 'expenseId'.
+    // If so, need to add 'expenseId' field in Expenses schema.
+export const REMOVE_EXPENSE = gql`
+    mutation removeExpense($expenseId: String!) {
+        removeExpense(expenseId: $expenseId) {
+            username
+            email
+            password
+            expenses {
+                name
+                expense
+            }
         }
     }
 `;
-// Mutation to use if we decide to remove an expense via 'expenseId'.
-    // If so, need to add 'expenseId' field in Expenses schema.
-// export const REMOVE_EXPENSE = gql`
-//     mutation removeExpense($expenseId: String!) {
-//         removeExpense(expenseId: $expenseId) {
-//             username
-//             email
-//             password
-//             expenses {
-//                 name
-//                 expense
-//             }
-//         }
-//     }
-// `
