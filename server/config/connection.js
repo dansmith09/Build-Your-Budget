2f6ca3c5936ba42afa4a314341fc90407b237d68
dansmith09 <dansmith_09@hotmail.com>
2022-10-17 21:16:17 +1100

    changed mongoose URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect('mongodb://localhost/buildyourbudget' || process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/buildyourbudget', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
