af649b49305c24b7aca0923a30ac87dffb7aa1df
brake <riaconoo@icloud.com>
2022-10-18 17:29:19 +1100

    added cards to homepage & styled to be responsive

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,20 +3,42 @@ import React from 'react';
 export default function Home(){
     return (
         <section>
-            <div>
-                <div className="element-one-container">
-                    <h1 className="element-one-header">
-                        The Next <br className="sm:block hidden" />{" "}
-                        <span className="text-gradient">Generation</span>{" "}
-                    </h1>
-                    {/* <div
-                        className="ss:flex hidden md:mr-4 mr-0">
-                    </div> */}
-                </div>
-                <h1 className="element-one-header">
-                    Expense Planner.
+            <div className='home-header-container'>
+                <h1>
+                    The Next Generation Expense Planner
                 </h1>
             </div>
+            <section className='cards-section'>
+            <h1> Expense </h1>
+                <div className='container'>
+                    <div className='cards'>
+                        <div className='card-content'>
+                            <h3> More test content</h3>
+                            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum,
+                            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                            </p>
+                        </div>
+                    </div>
+                    <div className='cards'>
+                        <div className='card-content'>
+                            <h3> More test content</h3>
+                            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum,
+                            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                            </p>
+                        </div>
+                    </div>
+                    <div className='cards'>
+                        <div className='break-section'></div>
+                        <div className='card-content'>
+                            <h3> More test content</h3>
+                            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum,
+                            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </section>
         
     )
