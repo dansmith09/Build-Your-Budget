bce956b0edb9421073321577413786d6a78bb7b3
dansmith09 <dansmith_09@hotmail.com>
2022-10-19 15:03:03 +1100

    Bar and Doughnut charts added and stiled. Small bug on homepage fixed

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ import Dashboard from './components/pages/Dashboard';
 import Donate from './components/pages/Donate';
 import Login from './components/pages/Login';
 import Signup from './components/pages/Signup';
-import LineChart from './charts/LineChart'
 
 const httpLink = createHttpLink({
     uri: '/graphql',
@@ -51,7 +50,6 @@ function App() {
                         <Route path="/donate" element={<Donate />}/>
                         <Route path="/login" element={<Login />}/>
                         <Route path="/signup" element={<Signup />}/>
-                        <Route path="/chart" element={<LineChart />}/>
                     </Routes>
                 </div>
             </Router>
