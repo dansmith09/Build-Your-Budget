7898aaa528d5faf270524a15c269f56072a64e10
dansmith09 <dansmith_09@hotmail.com>
2022-10-20 00:01:11 +1100

    need to figure out how to await data

--- client/src/charts/LineChart.js
+++ client/src/charts/LineChart.js
@@ -65,7 +65,7 @@ const LineChart = () => {
             setDisposableIncome(data.me.totalIncomes - data.me.totalExpenses)
             console.log(data, disposableIncome, data)
          }
-    },[])
+    },[data, disposableIncome])
 
     console.log(disposableIncome)
 
