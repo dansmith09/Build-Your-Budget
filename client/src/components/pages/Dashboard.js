441f3987c74724f543292406263ddfe87b5c9a6f
brake <riaconoo@icloud.com>
2022-10-20 14:51:10 +1100

    changed input form to be on the left

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -104,6 +104,38 @@ function AddExpense(props) {
                 <h1 className='dash-header'>Expense Planner</h1>
             </div>
             <section className='dash-form-container'>
+            <div className='dashboard-container'>
+                    <form onSubmit={handleIncomeFormSubmit}>
+                        <div>
+                            <p className='form-head'> Add an Income</p>
+                        </div>
+                        <div>
+                        <label htmlFor="name"></label>
+                            <input
+                                className='form-input'
+                                onChange={handleIncomeChange}
+                                type="text" 
+                                id="name" 
+                                name="name" 
+                                placeholder="income"
+                            >
+                            </input>
+                        </div>
+                        <div>
+                        <label htmlFor="amount"></label>
+                            <input
+                                className='form-input'
+                                onChange={handleIncomeChange}
+                                type="text" 
+                                id="amount" 
+                                name="amount" 
+                                placeholder="amount"
+                            >
+                            </input>
+                        </div>
+                    <button className='input-button' type="submit">add</button>
+                </form>
+            </div>
                 <div className='dashboard-container'>
                     <form onSubmit={handleExpenseFormSubmit}>
                         <div className='form-head'>
@@ -136,41 +168,22 @@ function AddExpense(props) {
                         <button className='input-button' type="submit">add</button>
                     </form>
                 </div>
-                <div className='dashboard-container'>
-                    <form onSubmit={handleIncomeFormSubmit}>
-                        <div>
-                            <p className='form-head'> Add an Income</p>
-                        </div>
-                        <div>
-                        <label htmlFor="name"></label>
-                            <input
-                                className='form-input'
-                                onChange={handleIncomeChange}
-                                type="text" 
-                                id="name" 
-                                name="name" 
-                                placeholder="income"
-                            >
-                            </input>
-                        </div>
-                        <div>
-                        <label htmlFor="amount"></label>
-                            <input
-                                className='form-input'
-                                onChange={handleIncomeChange}
-                                type="text" 
-                                id="amount" 
-                                name="amount" 
-                                placeholder="amount"
-                            >
-                            </input>
-                        </div>
-                    <button className='input-button' type="submit">add</button>
-                </form>
-            </div>
+                
             </section>
             <section>
                 <div className='dashboard-cards'>
+                <div className='dashboard-card'>
+                    <h2 className='dashboard-card-header'> Income(s) </h2>
+                    <br></br>
+                        {incomeList.map((income) => {
+                            return (
+                                <div className='dashboard-card-content'>
+                                    <p>{income.name} : ${income.amount}</p>
+                                </div>
+                            ) 
+                        })}
+                        <p className='dashboard-card-content'> Total: ${totalIncomes}</p>
+                    </div>
                     <div className='dashboard-card'>
                     <h2 className='dashboard-card-header'> Expenses </h2>
                     <br></br>
@@ -183,18 +196,7 @@ function AddExpense(props) {
                         })}
                         <p className='dashboard-card-content'> Total: ${totalExpenses}</p>
                     </div>
-                    <div className='dashboard-card'>
-                    <h2 className='dashboard-card-header'> Income(s) </h2>
-                    <br></br>
-                        {incomeList.map((income) => {
-                            return (
-                                <div className='dashboard-card-content'>
-                                    <p>{income.name} : ${income.amount}</p>
-                                </div>
-                            ) 
-                        })}
-                        <p className='dashboard-card-content'> Total: ${totalIncomes}</p>
-                    </div>
+                    
                     
                 </div>
                
